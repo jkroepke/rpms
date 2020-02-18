a69a49073232d6ee19da36c8100b2e55849fe4b4
Jan-Otto Kröpke <mail@jkroepke.de>
2020-02-18 17:30:33 +0100

    Fixed search.sh in GH actions

--- scripts/search.sh
+++ scripts/search.sh
@@ -2,4 +2,5 @@
 
 set -euo pipefail
 
-exec docker run --rm -ti fedora:rawhide bash -c "yum info git | grep Source | awk '{ print \$3 }' | tr -d '[:space:]'"
+exec docker run --rm fedora:rawhide \
+  bash -c "yum info git | grep Source | awk '{ print \$3 }' | tr -d '[:space:]'"
